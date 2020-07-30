983d214f6eebd46cc4852a68fdd4c9bb1a7c1db2
Riday <ridayshah2001@gmail.com>
2020-07-30 10:00:48 +0530

    Add: main file
    - This file has the entire backend structured.
    - Please do not modify if you don't know what you're doing.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Dependencies section.
+const logger = require('morgan');
+const path = require('path');
+const fs = require('fs');
+
+// Get the port.
+const port = process.env.PORT.replace(/\s/g, '') ?? 80;
+
+// Initialize the backend app.
+const express = require('express');
+const app = express();
+
+// Set some basic stuff.
+app.use(logger('dev'));
+app.use(express.static(path.join(`${__dirname}/public`)));
+
+// Handle routes.
+app.get('/', (_, res) => res.sendFile(path.join(`${__dirname}/pages/index.html`)));
+app.use((req, res) => {
+	// Send the file if it exists, or send the 404 error page.
+	const filePath = path.join(`${__dirname}/pages/${req.path}.html`);
+	if (fs.existsSync(filePath)) return res.sendFile(filePath);
+	return res.sendFile(path.join(`${__dirname}/pages/404.html`));
+});
+
+// Start the app by listening to the port.
+app.listen(port, () => console.log(`Listening on http://localhost${port === '80' ? '/' : `:${port}/`}`));
