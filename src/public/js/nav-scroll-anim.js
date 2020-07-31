f84ae5597055d2c99f2e064766f0daabe8beb649
Riday <ridayshah2001@gmail.com>
2020-07-31 11:55:37 +0530

    Fix: prettier

--- src/public/js/nav-scroll-anim.js
+++ src/public/js/nav-scroll-anim.js
@@ -1,22 +1,22 @@
 /* jshint esversion: 11 */
 
-//NavBar Animation
-const navSlide= () => {
+// NavBar Animation
+const navSlide = () => {
 	const burger = document.querySelector('#burger');
 	const nav = document.querySelector('.nav-links');
 
 	burger.addEventListener('click', () => {
-		//Toggle Nav
+		// Toggle Nav
 		nav.classList.toggle('open');
-		//Burger Animation
+		// Burger Animation
 		burger.classList.toggle('toggle');
 	});
 };
 navSlide();
 
 
-//NavBar Scroll Animation
-window.addEventListener("scroll",function(){
-    let nav=document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
+// NavBar Scroll Animation
+window.addEventListener('scroll', () => {
+	const nav = document.querySelector('nav');
+	nav.classList.toggle('sticky', window.scrollY > 0);
 });
